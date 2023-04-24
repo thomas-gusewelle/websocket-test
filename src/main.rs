1f5343efbd518da6622ce9bc39d4dc1d0f935c9e
Thomas Gusewelle <tgusewelle@gkwmedia.com>
2023-04-23 20:52:43 -0500

    wpi

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,55 @@
 use std::collections::HashSet;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
-use axum::extract::ws;
-use axum::{response::IntoResponse, routing::get, Json, Router};
+use axum::extract::{ws::{Message, WebSocket, WebSocketUpgrade}, State}
+use axum::{response::{IntoResponse, Html}, routing::get, Json, Router};
 // use axum_macros::debug_handler;
 use serde::{Deserialize, Serialize};
 use tokio::sync::broadcast;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+struct AppState {
+    user_set: Mutex<HashSet<String>>,
+    tx: broadcast::Sender<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let users = Mutex::new(HashSet::new());
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_chat=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let user_set = Mutex::new(HashSet::new());
     let (tx, _rx) = broadcast::channel(100);
 
-    let app = Router::new().route("/user", get(get_user));
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
+    let app_state = Arc::new(AppState { user_set, tx });
+
+    let app = Router::new()
+        // .route("/", get(index))
+        .route("/websocket", get(websocket_handler))
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
         .unwrap();
+    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
-#[derive(Debug, Serialize, Clone)]
-struct User {
-    id: usize,
-    name: String,
-    age: i32,
-}
+async fn websocket_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    ws.on_upgrade(|socket| websocket(socket, state))
+} 
 
-async fn get_user() -> impl IntoResponse {
-    Json(User {
-        id: 1,
-        name: "Thomas".to_string(),
-        age: 9,
-    })
+async fn websocket(stream: WebSocket, state: Arc<AppState>) {
+    // let (mut sender, mut reciever) = stream.split();
+
+    let mut rx = state.tx.subscribe();
+
+    let msg = format!("Someone Joined");
+    tracing::debug!("{}", msg);
+    let _ = state.tx.send(msg);
 }
